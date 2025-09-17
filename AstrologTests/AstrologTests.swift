f71ab38fab0ef6bc620a6668f4d64c01933707b6
Sergyoga2 <a1040798s@gmail.com>
2025-09-17 11:22:57 +0300

    Initial Commit

---
+++ AstrologTests/AstrologTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstrologTests.swift
+//  AstrologTests
+//
+//  Created by Sergey on 17.09.2025.
+//
+
+import Testing
+@testable import Astrolog
+
+struct AstrologTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
